b846cb8e68f88cd04ddf872c247ecbaeaa48ad3f
Luiz Ângelo Martins <Luiz ngelo Martins@LuizAngelo>
2021-01-11 20:50:46 -0200

    Commit feito para registrar o início do projeto:
    	- Criação da classe para calculos,
    	- Criação da classe que gera a lista de moedas disponiveis,
    	- Criação do "form" de exibição geral do programa.
    	- Implementação do projeto no repositório git.

---
+++ src/funcoes/Lista.java
@@ -0,0 +1,21 @@
+
+
+
+    // [ "CRIADOR: LUIZ ÂNGELO MARTINS" ]:
+
+
+
+// PACOTE DA CLASSE:
+package funcoes;
+
+
+// MÉTODO PRINCIPAL DA CLASSE:
+public class Lista {
+    
+    // IMPLEMENTE SEU CÓDIGO AQUI!
+    
+}
+
+
+
+
